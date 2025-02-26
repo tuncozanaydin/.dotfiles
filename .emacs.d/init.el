f23e445ef6bb3e30f6969a29d1ea51a01ed4c91b
toa <tuncozanaydin@gmail.com>
2025-02-26 17:49:02 +0100

    initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,37 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+;; Enable if packages should be pulled from github        
+;;(unless (package-installed-p 'vc-use-package)
+;;  (package-vc-install "https://github.com/slotThe/vc-use-package"))
+;;(require 'vc-use-package)
+
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+  
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(welcome-dashboard))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
