cc107ac33fcd2342f81c9b53e82f273ee0ad4f7d
toa <tuncozanaydin@gmail.com>
2025-03-11 16:59:00 +0100

    better vertico and small fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,5 +39,22 @@
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (* 10 1024 1024)) ;; 10MB
-            (setq gc-cons-percentage 0.1)
-            (message "GC restored to normal settings")))
+            (setq gc-cons-percentage 0.1)))
+;;            (message "GC restored to normal settings")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons conda corfu corfu-terminal dashboard doom-modeline
+		   doom-themes esup evil-org ligature marginalia
+		   no-littering orderless org-roam splash-screen
+		   treemacs vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
