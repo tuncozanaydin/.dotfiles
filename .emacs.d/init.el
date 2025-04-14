5e439c6f60b31c31d6c496492a246d766855f10a
toa <tuncozanaydin@gmail.com>
2025-04-14 09:11:56 +0200

    small fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,11 +48,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons conda corfu corfu-terminal dashboard doom-modeline
-		   doom-themes esup evil-org ligature marginalia
-		   no-littering orderless org-roam solaire-mode
-		   splash-screen treemacs treemacs-evil vertico
-		   yaml-mode)))
+   '(all-the-icons cheat-sh conda corfu corfu-terminal dashboard
+		   doom-modeline doom-themes esup evil-org ligature
+		   marginalia markdown-mode no-littering orderless
+		   org-roam solaire-mode splash-screen treemacs
+		   treemacs-evil vertico yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
