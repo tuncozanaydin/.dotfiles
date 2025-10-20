9b82dcd822247888040be735d23b53e2d447a35d
toa <tuncozanaydin@gmail.com>
2025-10-20 14:37:26 +0200

    small changes and launch script

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+brave --app=http://teams.microsoft.com/v2/ &
+brave --app=http://mail.disneyresearch.com &
+brave --app=http://calendar.disneyresearch.com &
+brave --app=http://chatgpt.com &
+brave &
+spotify &
